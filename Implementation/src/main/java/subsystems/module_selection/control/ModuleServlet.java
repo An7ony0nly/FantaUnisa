d47b11642ad8f7011ef755109050310a0191ff3f
f-corona <f.corona@ik.me>
2026-01-13 11:30:06 +0100

    Altre servlet e DAO

--- Implementation/src/main/java/subsystems/module_selection/control/ModuleServlet.java
+++ Implementation/src/main/java/subsystems/module_selection/control/ModuleServlet.java
@@ -1,4 +1,43 @@
 package subsystems.module_selection.control;
 
-public class ModuleServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import subsystems.module_selection.model.Module;
+
+import java.io.IOException;
+import java.util.List;
+
+
+@WebServlet("/modules")
+public class ModuleServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        List<Module> modules = Module.getValidModules();
+
+        response.setContentType("application/json;charset=UTF-8");
+
+        StringBuilder json = new StringBuilder();
+        json.append("[");
+        for (int i = 0; i < modules.size(); i++) {
+            Module m = modules.get(i);
+            json.append("{")
+                    .append("\"id\":\"").append(m.getId()).append("\",")
+                    .append("\"difensori\":").append(m.getDifensori()).append(",")
+                    .append("\"centrocampisti\":").append(m.getCentrocampisti()).append(",")
+                    .append("\"attaccanti\":").append(m.getAttaccanti())
+                    .append("}");
+            if (i < modules.size() - 1) {
+                json.append(",");
+            }
+        }
+        json.append("]");
+
+        response.getWriter().write(json.toString());
+    }
 }
